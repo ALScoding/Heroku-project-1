0ff7082e413816450f49c72758eab9717a57f363
Alexander Schalk <Alexanderlschalk@gmail.com>
2020-09-24 04:20:01 -0500

    add project files

---
+++ js/script.js
@@ -0,0 +1,451 @@
+// Clubs constant object
+const cardsArrayC = [
+  {
+    name: '2 of clubs',
+    facevalue: 'two',
+    img: 'cards_png/2C.png'
+  },
+  {
+    name: '3 of clubs',
+    facevalue: 'three',
+    img: 'cards_png/3C.png'
+  },
+  {
+    name: '4 of clubs',
+    facevalue: 'four',
+    img: 'cards_png/4C.png'
+  },
+  {
+    name: '5 of clubs',
+    facevalue: 'five',
+    img: 'cards_png/5C.png'
+  },
+  {
+    name: '6 of clubs',
+    facevalue: 'six',
+    img: 'cards_png/6C.png'
+  },
+  {
+    name: '7 of clubs',
+    facevalue: 'seven',
+    img: 'cards_png/7C.png'
+  },
+  {
+    name: '8 of clubs',
+    facevalue: 'eight',
+    img: 'cards_png/8C.png'
+  },
+  {
+    name: '9 of clubs',
+    facevalue: 'nine',
+    img: 'cards_png/9C.png'
+  },
+  {
+    name: '10 of clubs',
+    facevalue: 'ten',
+    img: 'cards_png/10C.png'
+  },
+  {
+    name: 'jack of clubs',
+    facevalue: 'jack',
+    img: 'cards_png/JC.png'
+  },
+  {
+    name: 'queen of clubs',
+    facevalue: 'queen',
+    img: 'cards_png/QC.png'
+  },
+  {
+    name: 'king of clubs',
+    facevalue: 'king',
+    img: 'cards_png/KC.png'
+  }
+]
+
+// Spades constant object
+const cardsArrayS = [
+  {
+    name: '2 of spades',
+    facevalue: 'two',
+    img: 'cards_png/2S.png'
+  },
+  {
+    name: '3 of spades',
+    facevalue: 'three',
+    img: 'cards_png/3S.png'
+  },
+  {
+    name: '4 of spades',
+    facevalue: 'four',
+    img: 'cards_png/4S.png'
+  },
+  {
+    name: '5 of spades',
+    facevalue: 'five',
+    img: 'cards_png/5S.png'
+  },
+  {
+    name: '6 of spades',
+    facevalue: 'six',
+    img: 'cards_png/6S.png'
+  },
+  {
+    name: '7 of spades',
+    facevalue: 'seven',
+    img: 'cards_png/7S.png'
+  },
+  {
+    name: '8 of spades',
+    facevalue: 'eight',
+    img: 'cards_png/8S.png'
+  },
+  {
+    name: '9 of spades',
+    facevalue: 'nine',
+    img: 'cards_png/9S.png'
+  },
+  {
+    name: '10 of spades',
+    facevalue: 'ten',
+    img: 'cards_png/10S.png'
+  },
+  {
+    name: 'jack of spades',
+    facevalue: 'jack',
+    img: 'cards_png/JS.png'
+  },
+  {
+    name: 'queen of spades',
+    facevalue: 'queen',
+    img: 'cards_png/QS.png'
+  },
+  {
+    name: 'king of spades',
+    facevalue: 'king',
+    img: 'cards_png/KS.png'
+  }
+]
+
+// Hearts constant object
+const cardsArrayH = [
+  {
+    name: '2 of hearts',
+    facevalue: 'two',
+    img: 'cards_png/2H.png'
+  },
+  {
+    name: '3 of hearts',
+    facevalue: 'three',
+    img: 'cards_png/3H.png'
+  },
+  {
+    name: '4 of hearts',
+    facevalue: 'four',
+    img: 'cards_png/4H.png'
+  },
+  {
+    name: '5 of hearts',
+    facevalue: 'five',
+    img: 'cards_png/5H.png'
+  },
+  {
+    name: '6 of hearts',
+    facevalue: 'six',
+    img: 'cards_png/6H.png'
+  },
+  {
+    name: '7 of hearts',
+    facevalue: 'seven',
+    img: 'cards_png/7H.png'
+  },
+  {
+    name: '8 of hearts',
+    facevalue: 'eight',
+    img: 'cards_png/8H.png'
+  },
+  {
+    name: '9 of hearts',
+    facevalue: 'nine',
+    img: 'cards_png/9H.png'
+  },
+  {
+    name: '10 of hearts',
+    facevalue: 'ten',
+    img: 'cards_png/10H.png'
+  },
+  {
+    name: 'jack of hearts',
+    facevalue: 'jack',
+    img: 'cards_png/JH.png'
+  },
+  {
+    name: 'queen of hearts',
+    facevalue: 'queen',
+    img: 'cards_png/QH.png'
+  },
+  {
+    name: 'king of hearts',
+    facevalue: 'king',
+    img: 'cards_png/KH.png'
+  }
+]
+
+// Diamonds constant object
+const cardsArrayD = [
+  {
+    name: '2 of diamonds',
+    facevalue: 'two',
+    img: 'cards_png/2D.png'
+  },
+  {
+    name: '3 of diamonds',
+    facevalue: 'three',
+    img: 'cards_png/3D.png'
+  },
+  {
+    name: '4 of diamonds',
+    facevalue: 'four',
+    img: 'cards_png/4D.png'
+  },
+  {
+    name: '5 of diamonds',
+    facevalue: 'five',
+    img: 'cards_png/5D.png'
+  },
+  {
+    name: '6 of diamonds',
+    facevalue: 'six',
+    img: 'cards_png/6D.png'
+  },
+  {
+    name: '7 of diamonds',
+    facevalue: 'seven',
+    img: 'cards_png/7D.png'
+  },
+  {
+    name: '8 of diamonds',
+    facevalue: 'eight',
+    img: 'cards_png/8D.png'
+  },
+  {
+    name: '9 of diamonds',
+    facevalue: 'nine',
+    img: 'cards_png/9D.png'
+  },
+  {
+    name: '10 of diamonds',
+    facevalue: 'ten',
+    img: 'cards_png/10D.png'
+  },
+  {
+    name: 'jack of diamonds',
+    facevalue: 'jack',
+    img: 'cards_png/JD.png'
+  },
+  {
+    name: 'queen of diamonds',
+    facevalue: 'queen',
+    img: 'cards_png/QD.png'
+  },
+  {
+    name: 'king of diamonds',
+    facevalue: 'king',
+    img: 'cards_png/KD.png'
+  }
+]
+
+//*******************************************************//
+
+let gameGrid,
+  message,
+  backcolor,
+  redcolor = false
+
+// color of suits is randomly chosen
+var suitcolor = Math.round(Math.random())
+
+// set up the grid, add card suits with the same color,
+// message that mentions the color, and back color
+suitcolor == 0
+  ? ((gameGrid = cardsArrayC.concat(cardsArrayS)),
+    (message = 'Match the black cards!'),
+    (backcolor = `url('cards_png/gray_back.png')`))
+  : ((gameGrid = cardsArrayH.concat(cardsArrayD)),
+    (message = 'Match the red cards!'),
+    (backcolor = `url('cards_png/red_back.png')`),
+    (redcolor = true))
+
+console.log(message)
+document.getElementById('message').innerHTML = message
+
+// text color is also changed
+document.getElementById('message').style.color =
+  redcolor === true ? 'red' : 'black'
+document.getElementById('attempts').style.color =
+  redcolor === true ? 'red' : 'black'
+
+// randomize the game grid on each refresh
+gameGrid.sort(() => 0.5 - Math.random())
+
+let firstGuess = '',
+  secondGuess = '',
+  count = 0,
+  pairs = 0,
+  attempts = 0,
+  previousTarget = null,
+  delay = 750
+
+// grab the div with an id of game
+const game = document.getElementById('game')
+
+// create a section with a class of grid
+const grid = document.createElement('section')
+grid.setAttribute('class', 'grid')
+
+// append the grid section to the game div
+game.appendChild(grid)
+
+// for each item in the cardsArray arrays...
+gameGrid.forEach(item => {
+  // create card element with the name dataset
+  const card = document.createElement('div')
+  card.classList.add('card')
+  card.dataset.name = item.name
+
+  // create back of card
+  const back = document.createElement('div')
+  back.classList.add('back')
+  back.style.backgroundImage = backcolor
+
+  // apply a card class to that div
+  card.classList.add('card')
+
+  // set the data-name attribute of the div to the cardsArray name
+  card.dataset.name = item.name
+  card.dataset.facevalue = item.facevalue
+
+  // create front of card, which contains the image
+  const front = document.createElement('div')
+  front.classList.add('front')
+  front.style.backgroundImage = `url(${item.img})`
+
+  // append card to grid, and back and front to each card
+  grid.appendChild(card)
+  card.appendChild(back)
+  card.appendChild(front)
+})
+
+// whenever the selected cards share the same value
+const match = () => {
+  var selected = document.querySelectorAll('.selected')
+
+  // back of cards will become green and cannot be flipped over again
+  selected.forEach(card => {
+    card.classList.add('match')
+    var innerContent = card.innerHTML
+    card.innerHTML =
+      redcolor === true
+        ? innerContent.replace('red_back.png', 'green_back.png')
+        : innerContent.replace('gray_back.png', 'green_back.png')
+  })
+
+  // increment pairs and mention in console
+  pairs++
+  console.log(
+    "The face values match. That's " +
+      pairs +
+      ' pair' +
+      (pairs > 1 ? 's' : '') +
+      '!'
+  )
+  // change message and text color when all 12 pairs are found
+  if (pairs === 12) {
+    document.getElementById('message').innerHTML = 'You Finished!'
+    document.getElementById('message').style.color = 'gold'
+    document.getElementById('attempts').style.color = 'gold'
+    playSound3()
+  }
+}
+
+// code always executes after two cards are chosen
+const resetGuesses = () => {
+  // reset guesses and count, increment attempts
+  ;(firstGuess = ''),
+    (secondGuess = ''),
+    (count = 0),
+    (previousTarget = null),
+    attempts++
+
+  // unselect cards
+  var selected = document.querySelectorAll('.selected')
+  selected.forEach(card => {
+    card.classList.remove('selected')
+  })
+
+  // update the attempts number
+  document.getElementById('attempts').innerHTML = 'Attempts: ' + attempts
+
+  console.log('')
+}
+
+// three functions that each play a sound effect
+function playSound1 () {
+  var audio1 = new Audio('sounds/flip.wav')
+  audio1.play()
+}
+
+function playSound2 () {
+  var audio2 = new Audio('sounds/matched.wav')
+  audio2.play()
+}
+
+function playSound3 () {
+  var audio3 = new Audio('sounds/tada.wav')
+  audio3.play()
+}
+
+// add event listener to grid
+grid.addEventListener('click', event => {
+  const clicked = event.target
+
+  // invalid clicks
+  if (
+    clicked.nodeName === 'section' ||
+    clicked === previousTarget ||
+    clicked.parentNode.dataset.name === undefined ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return
+  }
+
+  // flipping card sound whenever click is valid
+  playSound1()
+
+  if (count < 2) {
+    count++
+    // assign the guess, using the face value
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.facevalue
+    } else {
+      secondGuess = clicked.parentNode.dataset.facevalue
+    }
+    // name of card is mentioned in console
+    // selected card cannot be selected again
+    console.log('The ' + clicked.parentNode.dataset.name + ' was selected.')
+    clicked.parentNode.classList.add('selected')
+
+    // if both guesses are not empty...
+    if (firstGuess !== '' && secondGuess !== '') {
+      // and the first guess matches the second guess...
+      if (firstGuess === secondGuess) {
+        playSound2()
+        // there is a brief delay before cards are flipped over again
+        setTimeout(match, delay)
+        setTimeout(resetGuesses, delay)
+      } else {
+        setTimeout(resetGuesses, delay)
+      }
+    }
+  }
+})
